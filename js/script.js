70a4f6194c919dc4095402d4d1bde605c408ad81
Sandrajasmin <sandra-jasmin@hotmail.com>
2022-02-02 11:36:11 +0100

    addd

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//Question 1
+//Create a function called receivingFunction which accepts one argument. 
+//Inside this function check if the argument is a function, and if it is, 
+//call the function.
+
+//Create another function called callbackFunction which logs the string:
+// "I am a callback function".
+
+//Call receivingFunction and pass callbackFunction into it as an argument.
+
+function receivingFunction (someArgument) {
+
+    console.log("the "typeof someArgument);
+}
